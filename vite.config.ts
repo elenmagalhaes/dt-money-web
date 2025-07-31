02e59e2af7a252d7a0b8e9c0792dbea2600bacbc
Elen Magalhães <fromeleen@gmail.com>
2025-07-31 20:30:14 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"assets": "/src/assets",
+			"components": "/src/components",
+			"styles": "/src/styles",
+			"pages": "/src/pages",
+		},
+	},
+})
